5dcd90e5772d3c85b3fd8432bf8e0ae58390795e
Arnav Ghosh <ag983@cornell.edu>
2019-03-23 13:13:35 -0400

    vectors loaded automatically

--- src/torchtext_process.py
+++ src/torchtext_process.py
@@ -8,18 +8,22 @@ import os
 import torch
 import torch.nn as nn
 from torchtext import data, datasets
+from torchtext.vocab import Vectors
 
 # CONSTANTS
 SOS = 'SOS'
 EOS = 'EOS'
 
+VECTOR_MODEL = Vectors(name='glove.6B.300d.txt', cache=os.path.join(os.getcwd(), "data"))
+
 FIELDS_ROOT_PATH = os.path.join("data")
 SRC_FIELD_PATHS = ("src", os.path.join(FIELDS_ROOT_PATH, "src"))
-TGT_FIELD_PATHS = ("src", os.path.join(FIELDS_ROOT_PATH, "src"))
+TGT_FIELD_PATHS = ("tgt", os.path.join(FIELDS_ROOT_PATH, "tgt"))
 
 def whitespace_tokenizer(text):
     return text.split(" ")
 
+# TODO : Pass Vector Model
 def load_torchtext_datasets(data_path, rel_train, rel_val, rel_test):
     if not os.path.exists(SRC_FIELD_PATHS[1]):
         SRC = data.Field(
@@ -51,13 +55,15 @@ def load_torchtext_datasets(data_path, rel_train, rel_val, rel_test):
 
     # Build SRC and TGT Vocab if not preloaded & save fields
     if not os.path.exists(SRC_FIELD_PATHS[1]):
-        SRC.build_vocab(train) #TODO: Add vectors? How do we ensure that made-up-tokens are there?
+        SRC.build_vocab(train, vectors = VECTOR_MODEL) #TODO: Add vectors? How do we ensure that made-up-tokens are there?
+        #SRC.build_vocab(train)
 
         with open(SRC_FIELD_PATHS[1], 'wb+') as dill_file:
             dill.dump(SRC, dill_file)
 
     if not os.path.exists(TGT_FIELD_PATHS[1]):
-        TGT.build_vocab(train) #TODO: Add vectors? How do we ensure that made-up-tokens are there?
+        TGT.build_vocab(train, vectors = VECTOR_MODEL) #TODO: Add vectors? How do we ensure that made-up-tokens are there?
+        TGT.build_vocab(train)
 
         with open(TGT_FIELD_PATHS[1], 'wb+') as dill_file:
             dill.dump(TGT, dill_file)
