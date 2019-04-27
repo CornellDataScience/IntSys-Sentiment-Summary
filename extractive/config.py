f04e865887e996c4914a377d711d1f6bd6403709
Matt Bayer <mattbayer@dhcp-rhodes-1090.redrover.cornell.edu>
2019-04-27 16:26:33 -0400

    removed unnecessary config parameter

--- extractive/config.py
+++ extractive/config.py
@@ -1,7 +1,6 @@
 config = {"density_parameter": 0.04, 
           "minimum_samples": 4,
           "min_clusters": 5,
-          "min_num_candidates": 75,
           "min_chars_per_sentence": 35,
           "min_num_candidates": 100,
           "max_acceptable_clusters": 30 }
