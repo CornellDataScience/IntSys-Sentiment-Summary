5e91c5d4e67b0e1e93f3c4e106c0a1f6c7c01f58
Arnav Ghosh <ag983@cornell.edu>
2019-04-30 00:34:35 -0400

    Soft Cosine Similarity Functions complete

--- siamese/scm_cluster_analysis.py
+++ siamese/scm_cluster_analysis.py
@@ -6,12 +6,15 @@ from gensim import corpora
 
 from gensim.corpora import Dictionary
 from gensim.test.utils import common_texts
-from gensim.similarities import SoftCosineSimilarity
+from gensim.similarities import SoftCosineSimilarity, SparseTermSimilarityMatrix
+from gensim.models import WordEmbeddingSimilarityIndex
 
 import utils
 
 # ================== CONSTANTS ==================
-TOP_SIMS = 5
+TOP_SIMS = 3
+
+# ============== CORPORA + INDEX GEN. ==============
 
 def create_random_corpus(sentences, labels, num_sents):
     num_clusters = len(set(labels))
@@ -27,7 +30,7 @@ def create_random_corpus(sentences, labels, num_sents):
     return [sentences[i] for i in rand_idxs]
 
 def generate_corpus(sentences):
-    documents = list(map(sentences, lambda x : [w for w in sentencs.split() if w not in utils.STOP_WORDS]))
+    documents = list(map(lambda x : [w for w in x.split() if w not in utils.STOP_WORDS], sentences))
     dictionary = corpora.Dictionary(documents)
     bow_corpus = [dictionary.doc2bow(doc) for doc in documents]
 
@@ -40,9 +43,14 @@ def get_sim_index(wv_model, bow_corpus, dictionary):
 
     return docsim_index
 
+# ============== SENTENCE SIM. ==============
+
 def cluster_sent_sim(sent, sim_index, dictionary):
+    print("Here")
     sims = sim_index[dictionary.doc2bow(sent.split())] #[(<doc_idx>, <score>), ...]
     total = 0
     for s in sims[:TOP_SIMS]:
         total += s[1]
     return total / TOP_SIMS
+
+# Pick some closest to mean and some further away from mean and choose corpus as entire cluster
