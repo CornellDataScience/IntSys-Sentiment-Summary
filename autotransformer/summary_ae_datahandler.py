bf575b0c48a1b94bc0bb65f99477bd7fd2e84507
wesg52 <rwg97@cornell.edu>
2019-04-29 23:39:16 -0400

    decoding and import fix

--- autotransformer/summary_ae_datahandler.py
+++ autotransformer/summary_ae_datahandler.py
@@ -1,13 +1,14 @@
 from torchtext import data
-from transformer.flow import batch_size_fn_inference
-from transformer.my_iterator import MyIterator
-
+from torch.autograd import Variable
+from .transformer.functional import subsequent_mask
+from .transformer.flow import batch_size_fn_inference
+from .transformer.my_iterator import MyIterator
 
 
 def whitespace_tokenizer(text):
     return text.strip().split()
 
-def make_sentence_iterator(sent_list, device):
+def make_sentence_iterator(sent_list, batch_size):
     BOS_WORD = '<s>'
     EOS_WORD = '</s>'
     BLANK_WORD = '<blank>'
@@ -22,9 +23,29 @@ def make_sentence_iterator(sent_list, device):
 
     sent_data = torchtext.data.Dataset(examples, [('src', SRC)])
 
-    sent_iter = MyIterator(sent_data, batch_size=BATCH_SIZE, device=device, repeat=False,
+    sent_iter = MyIterator(sent_data, batch_size=batch_size, device=0, repeat=False,
                             sort_key=lambda x: len(x.src), batch_size_fn=batch_size_fn_inference, train=False)
 
     return sent_iter, SRC, BOS_WORD, EOS_WORD, BLANK_WORD, CLS_WORD
+
+def greedy_decode(model, encodings):
+    #TODO: batch this
+    candidates = []
+    for candidate in encodings:
+        ys = torch.ones(1, 1).fill_(trg_vocab.stoi['<s>']).type_as(src.data)
+        for i in range(max_len - 1):
+            out = model.decode(candidate.reshape(1,1,300), torch.ones(1,1,1).cuda(), Variable(ys), Variable(subsequent_mask(ys.size(1)).type_as(src.data)))
+            prob = model.generator(out[:, -1])
+            _, next_word = torch.max(prob, dim=1)
+            next_word = next_word.data[0]
+            ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+        sentence_words = []
+        for k in range(1, ys.size(1)):
+            sym = trg_vocab.itos[ys[0, k]]
+            if sym == EOS_WORD:
+                break
+            sentence_words.append(sym)
+        candidates.append(' '.join(sentence_words))
+    return candidates
 
     
