ca59bd2fbeb98b1ae518633b6fdb194fdd958b7c
matthewbayer <44325414+matthewbayer@users.noreply.github.com>
2019-05-04 15:18:11 -0400

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -223,11 +223,11 @@ if __name__ == "__main__":
     'autoencoder' : None,
     'ae_batchsize': 5000,
 
-    'density_parameter' : .04,
-    'minimum_samples': 4,
-    'min_clusters': 5,
-    'max_acceptable_clusters':30,
-    'min_num_candidates': 100,
+    'density_parameter' : 2,
+    'minimum_samples': 2,
+    'min_clusters': 50,
+    'max_acceptable_clusters':100,
+    'min_num_candidates': 250,
 
     'BERT_finetune_path' : 'bert_finetune/models/finetune_electronics_mae1.pt',
     'BERT_config_path' : 'bert_finetune/models/finetune_electronics_mae1config.json',
